1af1fdda03d8aaa1ab10d20814e0de28c8272b82
clickingmouse <clickingmouse@users.noreply.github.com>
2018-04-20 19:07:07 +0800

    render a component

---
+++ webpack.config.dev.js
@@ -0,0 +1,24 @@
+import path from 'path';
+
+export default {
+  devtool:'source-map',
+entry:path.join(__dirname, '/client/index.js'),
+output:{
+  filename:'bundle.js',
+path:'/'
+
+},
+module:{
+  rules: [
+    {
+      test: /\.js$/,
+      include: path.join(__dirname, 'client'),
+      loaders:['babel-loader']
+    }
+  ]
+},
+resolve : {
+  extensions:['*', '.js']
+}
+
+}
